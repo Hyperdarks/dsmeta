d38e1b5a53897833180b9468c4236e21da1cab7e
Felipe Guilherme <hyperdarks693@outlook.com>
2022-11-27 19:29:24 -0300

    Send notification

--- frontend/src/componentes/NotificationButton/index.tsx
+++ frontend/src/componentes/NotificationButton/index.tsx
@@ -1,10 +1,22 @@
+import axios from "axios";
 import icon from "../../assets/img/notification-icon.svg";
+import { BASE_URL } from "../../utilils/request";
 
 import './styles.css';
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function handleClick(id: number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(Response => {
+        console.log("SUCESSO");
+    });
+}
+function NotificationButton( {saleId}: Props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
