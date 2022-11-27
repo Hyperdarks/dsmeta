afc99ab24690b693ff956c930de0cfe815f971cf
Felipe Guilherme <hyperdarks693@outlook.com>
2022-11-27 18:49:42 -0300

    Axios, useEffect first request

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -34,8 +34,8 @@ import com.twilio.type.PhoneNumber;
 			
 			String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 			
-			String msg = "O Vendedor "+ sale.getSellerName() + " foi destaque em " + date
-					+" com um total de R$ " + String.format("%.2f", sale.getAmount());
+			String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
+				    + " com um total de R$ " + String.format("%.0f", sale.getAmount());
 			
 			Twilio.init(twilioSid, twilioKey);
 
